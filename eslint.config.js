57d9596d14a086480a17501c3b081d97f618a14e
TheRegzi <reginekornbakk@gmail.com>
2024-11-15 15:06:36 +0100

    Added lint and lint-fix

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+
+export default [
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-extra-semi": "error",
+    },
+  },
+];
