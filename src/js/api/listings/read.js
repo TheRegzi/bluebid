9378c9f10bb01a3b44cbdbee975c6ab99fbcbd62
TheRegzi <reginekornbakk@gmail.com>
2024-11-19 12:30:49 +0100

    Feat: Adding more styling to listings

--- src/js/api/listings/read.js
+++ src/js/api/listings/read.js
@@ -57,16 +57,16 @@ function renderListings(listings) {
         : listing.title;
 
     listingElement.innerHTML = `
-    <a href="/listing/index.html?id=${listing.id}" class="block">
-    <div class='flex flex-col justify-center mx-auto w-350 mb-10'>
-    <img src="${imageUrl}" alt="${imageAlt}" class='w-350 rounded-t-xl shadow-2xl'>
-        <div class='bg-secondary rounded-b-xl mb-2 p-3 shadow-lg'>
-            <h3 class='font-headingMd font-bold text-md'>${listing.title}</h3>
-            <p class='font-body'>Current Bid: ${listing.currentBid || 0} credits</p>
-        <div>
-    </div>
-    </a>
-    `;
+        <a href="/listing/index.html?id=${listing.id}" class="block">
+          <div class="flex flex-col justify-center mx-auto w-80 h-96 mb-10 shadow-xl">
+            <img src="${imageUrl}" alt="${imageAlt}" class="w-full h-4/5 object-cover rounded-t-xl shadow-2xl">
+            <div class="bg-secondary rounded-b-xl p-5 shadow-lg border-t border-accent2 flex-grow">
+              <h3 class="font-headingMd font-bold text-md text-shadow-lg">${listing.title}</h3>
+              <p class="font-body">Current Bid: ${listing.currentBid || 0} credits</p>
+            </div>
+          </div>
+        </a>
+      `;
     listingsContainer.appendChild(listingElement);
   });
 }
