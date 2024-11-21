54294fe3cdda7c2d164a13a6eefde153cc41d42b
TheRegzi <reginekornbakk@gmail.com>
2024-11-21 14:55:07 +0100

    Feat: added a function to change text of banner

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,17 @@
 import { fetchListings } from '../../api/listings/multipleListings';
 
 fetchListings();
+
+function updateBannerBasedOnAuth() {
+  const token = localStorage.getItem('userToken');
+  const bannerBody = document.getElementById('banner-body');
+  const bannerLink = document.getElementById('banner-link');
+
+  if (token) {
+    bannerBody.textContent =
+      'Search Through Thousands of Items and Uncover Hidden Treasures.';
+    bannerLink.innerHTML = `<a href='/search/index.html'>Search Here!</a>`;
+  }
+}
+
+updateBannerBasedOnAuth();
