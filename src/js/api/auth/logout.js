59ebf9415938a8ea5808dfcce13942e8a4245923
TheRegzi <reginekornbakk@gmail.com>
2024-12-05 13:26:21 +0100

    feat; added JS docs to logout

--- src/js/api/auth/logout.js
+++ src/js/api/auth/logout.js
@@ -1,3 +1,11 @@
+/**
+ * Attaches a 'click' event listener to the logout button that logs out the user.
+ * The user's 'userToken' and 'name' are removed from localStorage upon logout.
+ * After clearing the data, the user is redirected to the login page.
+ *
+ * @returns {void} This function does not return any value.
+ */
+
 export function logOutUser() {
   const logOutButtons = document.querySelectorAll('.logOut');
 
