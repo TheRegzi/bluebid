d2d881f83c68d491727bfcf02fc0a13cbea987dc
TheRegzi <reginekornbakk@gmail.com>
2024-12-06 13:18:51 +0100

    feat: added JS docs to editListing

--- src/js/api/listings/editListing.js
+++ src/js/api/listings/editListing.js
@@ -3,6 +3,18 @@ import { API_AUCTION_LISTINGS } from '../constants';
 import { displayError } from '../../UI/error';
 import { displayLoading, hideLoading } from '../../UI/loading';
 
+/**
+ * Tries to fetch an auction listing to use it in the 'populateInputs' function.
+ * It sends a GET request to the API_AUCTION_LISTINGS/listingId endpoint.
+ * It uses the `id` query parameter from the current page's URL to determine the listing ID.
+ * If successful, it returns the data in JSON format.
+ * If unsuccessful, it throws an error.
+ *
+ * @async
+ * @returns {Promise<Object|null>} - A promise that resolves to the listing data in JSON format, or `null` if the fetch fails or the ID is not found.
+ * @throws {Error} - Throws an error if the fetch fails.
+ */
+
 async function fetchListing() {
   const listingId = new URLSearchParams(window.location.search).get('id');
 
@@ -31,6 +43,14 @@ async function fetchListing() {
   }
 }
 
+/**
+ * Populates inputs to edit the auction listing, by using the data retrieved from the 'fetchListing' function.
+ * If the listing cannot be fetched, it displays an error with the 'displayError' function.
+ *
+ * @async
+ * @returns {Promise<void>} - Resolves when the form inputs are populated or an error is handled.
+ */
+
 async function populateInputs() {
   const listing = await fetchListing();
 
@@ -47,6 +67,21 @@ async function populateInputs() {
 }
 
 populateInputs();
+
+/**
+ * Updates a listing by sending a PUT request to the API_AUCTION_LISTINGS/listingId endpoint.
+ * It gathers the data from the input fields, and sends it as the updated version of the listing.
+ * If successful it returns the updated data in JSON format and redirects to the updated listing's page.
+ * If unsuccessful, it displays an error with the 'displayError' function.
+ *
+ * @param {string} listingId - The unique identifier of the listing to be deleted.
+ * @param {Object} data - The data object containing updated listing details.
+ * @param {string} data.title - The updated title of the listing.
+ * @param {string} data.description - The updated description of the listing.
+ * @param {Array<{url: string, alt: string}>} data.media - An array of media objects with URLs and alt texts for the listing.
+ * @returns {Promise<Object|null>} - A promise that resolves to the listing data in JSON format, or `null` if the update fails or the ID is not found.
+ * @throws {Error} - Throws an error if the update fails.
+ */
 
 export async function updateListing(listingId, { title, description, media }) {
   const apiUrl = `${API_AUCTION_LISTINGS}/${listingId}`;
@@ -88,6 +123,15 @@ export async function updateListing(listingId, { title, description, media }) {
     hideLoading();
   }
 }
+
+/**
+ * Initializes the submit button by adding a submit event listener.
+ * The event listener prevents the default form submission behavior, validates the presence of a listing ID,
+ * and takes the listingId and formData and passes it into the function 'updateListing' to update the listing.
+ * If no listing ID is found, an error message is displayed using the `displayError` function.
+ *
+ * @returns {Promise<void>} - This function does not return a value; it attaches an event listener to the form.
+ */
 
 export function initializeSubmitButton() {
   document
