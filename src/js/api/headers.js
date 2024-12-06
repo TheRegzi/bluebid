5a407ac527bff7648f95d96cefaec7bd96c70b63
TheRegzi <reginekornbakk@gmail.com>
2024-12-06 14:59:41 +0100

    feat: added JS docs to headers

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,3 +1,16 @@
+/**
+ * Constructs and returns the headers needed for API requests.
+ *
+ * This function sets the `'Content-Type': 'application/json'` header for all requests.
+ * If available, it also appends the API key and the user's authentication token from localStorage.
+ *
+ * - The API key (`X-Noroff-API-Key`) is imported from the .env file via import.meta.env.VITE_NOROFF_API_KEY;
+ * - The Authorization token (`Bearer token`) is retrieved from localStorage.
+ *
+ * @returns {Headers} An instance of the Headers object to be used in API requests.
+ *
+ */
+
 export async function headers() {
   const headers = new Headers();
 
