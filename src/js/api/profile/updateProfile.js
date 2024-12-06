52024b0d61dadde9bbf045fcfc68f517e0be873b
TheRegzi <reginekornbakk@gmail.com>
2024-12-06 13:33:50 +0100

    feat: added JS docs for updateProfile

--- src/js/api/profile/updateProfile.js
+++ src/js/api/profile/updateProfile.js
@@ -4,6 +4,19 @@ import { headers } from '../headers';
 import { displayError } from '../../UI/error';
 import { displayLoading, hideLoading } from '../../UI/loading';
 
+/**
+ * Populates form inputs by using the data retrieved from the 'fetchUserProfile' function.
+ * If no profile data, it displays an error message with the 'displayError' function.
+ *
+ * Form fields populated:
+ * - Bio input (`#update-bio`) with the user's bio.
+ * - Avatar URL input (`#update-image`) with the user's avatar URL.
+ * - Banner URL input (`#update-banner`) with the user's banner URL.
+ *
+ * @async
+ * @returns {Promise<void>} - Resolves when the form inputs are populated or an error is handled.
+ */
+
 export async function populateForms() {
   const profileData = await fetchUserProfile();
 
@@ -16,6 +29,25 @@ export async function populateForms() {
   document.getElementById('update-image').value = profileData.avatar.url || '';
   document.getElementById('update-banner').value = profileData.banner.url || '';
 }
+
+/**
+ * Updates user profile by sending a PUT request to the API_AUCTION_PROFILES/name endpoint.
+ * The function takes the username and collects the updated data (bio, avatar, and banner) from input fields and sends it as the updated profile information.
+ * If successful, it sends the data from the inputs as the updated version of the profile and redirects to the home page.
+ * If unsuccessful, it displays an error with the 'displayError' function.
+ *
+ * @param {string} name - The registered username of the logged in user.
+ * @param {Object} data - The data object containing updated profile details.
+ * @param {string} [data.bio] - The updated bio for the user profile.
+ * @param {Object} [data.avatar] - The updated avatar object containing its URL and optional alt text.
+ * @param {string} data.avatar.url - The URL of the avatar image.
+ * @param {string} [data.avatar.alt] - The alternative text for the avatar image.
+ * @param {Object} [data.banner] - The updated banner object containing its URL and optional alt text.
+ * @param {string} data.banner.url - The URL of the banner image.
+ * @param {string} [data.banner.alt] - The alternative text for the banner image.
+ * @returns {Promise<Object|null>} - A promise that resolves to the profile data in JSON format, or `null` if the update fails.
+ * @throws {Error} - Throws an error if the update fails.
+ */
 
 export async function updateProfile(name, { bio, avatar, banner }) {
   const apiUrl = `${API_AUCTION_PROFILES}/${name}`;
@@ -53,6 +85,14 @@ export async function updateProfile(name, { bio, avatar, banner }) {
     hideLoading();
   }
 }
+
+/**
+ * Initializes the submit button by adding a submit event listener. Prevents default form behavior.
+ * It collects the new formData and calls the `updateProfile` function with the logged-in user's username and the updated profile data.
+ * The username is retrieved from `localStorage` under the key `name`.
+ *
+ * @returns {void} - This function does not return a value; it sets up an event listener for the form submission.
+ */
 
 export function initializeSubmitButton() {
   document
