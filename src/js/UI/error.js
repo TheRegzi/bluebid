9e80b8b0c1a4c0ff3c47b4d7f090ed1d4efc2aee
TheRegzi <reginekornbakk@gmail.com>
2024-12-06 15:18:01 +0100

    feat: added JS docs to error

--- src/js/UI/error.js
+++ src/js/UI/error.js
@@ -1,3 +1,14 @@
+/**
+ * Displays an error message in the HTML element with the ID `error`.
+ * The message is styled and made visible to the user. This function is typically used
+ * to provide user feedback when an error occurs in other functions.
+ *
+ * - If the element with ID `error` is not found in the DOM, logs an error message to the console.
+ *
+ * @param {string} message - The error message to be displayed to the user.
+ * @returns {void} - This function does not return a value.
+ */
+
 export function displayError(message) {
   const errorElement = document.getElementById('error');
   if (errorElement) {
