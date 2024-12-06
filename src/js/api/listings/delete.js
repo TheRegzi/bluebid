50595181327b2af9031e08db2145e6ae3e9477f4
TheRegzi <reginekornbakk@gmail.com>
2024-12-06 12:53:04 +0100

    feat: added JS docs to  delete.js

--- src/js/api/listings/delete.js
+++ src/js/api/listings/delete.js
@@ -2,6 +2,17 @@ import { API_AUCTION_LISTINGS } from '../constants';
 import { headers } from '../headers';
 import { displayError } from '../../UI/error';
 
+/**
+ * Deletes a listing by sending a 'DELETE' request to the 'API_AUCTION_LISTINGS/listingId' endpoint.
+ * If successful, it logs a success message and redirects to the home page.
+ * If unsuccessful, it displays an error message with the 'displayError' function.
+ *
+ * @async
+ * @param {string} listingId - The unique identifier of the listing to be deleted.
+ * @returns {Promise<void>} - Resolves when the listing is deleted or an error is handled.
+ * @throws {Error} - Throws an error if the deletion fails.
+ */
+
 export async function deleteListing(listingId) {
   const apiUrl = `${API_AUCTION_LISTINGS}/${listingId}`;
 
@@ -13,7 +24,7 @@ export async function deleteListing(listingId) {
     });
 
     if (response.status === 204) {
-      displayError('Auction Listing deleted successfully.');
+      console.log('Auction Listing deleted successfully.');
       window.location.href = `/index.html`;
       return;
     } else {
