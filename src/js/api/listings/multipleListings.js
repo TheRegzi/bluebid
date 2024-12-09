8804bd7937eabccf53cbaceae4ec5f1cd5906885
TheRegzi <reginekornbakk@gmail.com>
2024-12-09 12:48:45 +0100

    feat: changed brightness on listing element

--- src/js/api/listings/multipleListings.js
+++ src/js/api/listings/multipleListings.js
@@ -96,7 +96,7 @@ function renderListings(listings) {
 
     listingElement.innerHTML = `
         <a href="/listing/index.html?id=${listing.id}" class="block group">
-          <div class="mx-auto w-80 sm:w-96 h-96 mb-10 transition-all group-hover:shadow-2xl group-hover:brightness-110 group-hover:scale-105 rounded-xl">
+          <div class="mx-auto w-80 sm:w-96 h-96 mb-10 transition-all group-hover:shadow-2xl brightness-95 group-hover:brightness-100 group-hover:scale-105 rounded-xl">
             <img src="${imageUrl}" alt="${imageAlt}" class="w-full h-4/5 object-cover rounded-t-xl shadow-2xl">
             <div class="bg-secondary rounded-b-xl p-5 shadow-lg border-t border-accent2 flex-grow">
               <h3 class="font-headingMd font-bold text-md text-shadow-lg">${truncateText(listing.title, 30)}</h3>
